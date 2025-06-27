6309ab7a25506d8b6db3725f9d0283f19defd72f
hypengw <hypengwip@gmail.com>
2025-06-27 11:26:41 +0000

    width provider margin and column

--- include/qml_material/util/width_provider.hpp
+++ include/qml_material/util/width_provider.hpp
@@ -9,14 +9,31 @@ class WidthProvider : public QObject {
     QML_ELEMENT
 
     Q_PROPERTY(qint32 width READ width NOTIFY widthChanged FINAL)
+    Q_PROPERTY(qint32 column READ column NOTIFY columnChanged FINAL)
     Q_PROPERTY(qint32 minimum READ minimum WRITE setMinimum NOTIFY minimumChanged FINAL)
+    Q_PROPERTY(qint32 leftMargin READ leftMargin WRITE setLeftMargin NOTIFY leftMarginChanged FINAL)
+    Q_PROPERTY(
+        qint32 rightMargin READ rightMargin WRITE setrightMargin NOTIFY rightMarginChanged FINAL)
     Q_PROPERTY(qint32 spacing READ spacing WRITE setSpacing NOTIFY spacingChanged FINAL)
     Q_PROPERTY(qint32 total READ total WRITE setTotal NOTIFY totalChanged FINAL)
 public:
     explicit WidthProvider(QObject* parent = nullptr);
 
+    Q_INVOKABLE qint32 calculateX(qint32 column) noexcept;
+
     auto          width() const noexcept -> qint32;
     Q_SIGNAL void widthChanged();
+
+    auto          column() const noexcept -> qint32;
+    Q_SIGNAL void columnChanged();
+
+    auto          leftMargin() const noexcept -> qint32;
+    void          setLeftMargin(qint32);
+    Q_SIGNAL void leftMarginChanged();
+
+    auto          rightMargin() const noexcept -> qint32;
+    void          setrightMargin(qint32);
+    Q_SIGNAL void rightMarginChanged();
 
     auto          minimum() const noexcept -> qint32;
     void          setMinimum(qint32);
@@ -33,10 +50,13 @@ public:
     Q_SLOT void refresh();
 
 private:
+    qint32 m_column;
     qint32 m_width;
     qint32 m_total;
     qint32 m_minimum;
     qint32 m_spacing;
+    qint32 m_left_margin;
+    qint32 m_right_margin;
 };
 
 } // namespace qml_material
--- src/util/width_provider.cpp
+++ src/util/width_provider.cpp
@@ -4,12 +4,20 @@
 namespace qml_material
 {
 WidthProvider::WidthProvider(QObject* parent)
-    : QObject(parent), m_width(0), m_total(0), m_minimum(160), m_spacing(0) {
+    : QObject(parent),
+      m_column(1),
+      m_width(0),
+      m_total(0),
+      m_minimum(160),
+      m_spacing(0),
+      m_left_margin(0),
+      m_right_margin(0) {
     connect(
         this, &WidthProvider::totalChanged, this, &WidthProvider::refresh, Qt::DirectConnection);
 }
 
 auto WidthProvider::width() const noexcept -> qint32 { return m_width; }
+auto WidthProvider::column() const noexcept -> qint32 { return m_column; }
 
 auto WidthProvider::minimum() const noexcept -> qint32 { return m_minimum; }
 void WidthProvider::setMinimum(qint32 v) {
@@ -18,6 +26,21 @@ void WidthProvider::setMinimum(qint32 v) {
         minimumChanged();
     }
 }
+auto WidthProvider::leftMargin() const noexcept -> qint32 { return m_left_margin; }
+void WidthProvider::setLeftMargin(qint32 v) {
+    if (m_left_margin != v) {
+        m_left_margin = v;
+        leftMarginChanged();
+    }
+}
+auto WidthProvider::rightMargin() const noexcept -> qint32 { return m_right_margin; }
+void WidthProvider::setrightMargin(qint32 v) {
+    if (m_right_margin != v) {
+        m_right_margin = v;
+        rightMarginChanged();
+    }
+}
+
 auto WidthProvider::total() const noexcept -> qint32 { return m_total; }
 void WidthProvider::setTotal(qint32 v) {
     if (m_total != v) {
@@ -33,24 +56,35 @@ void WidthProvider::setSpacing(qint32 v) {
     }
 }
 
+auto WidthProvider::calculateX(qint32 column) noexcept -> qint32 {
+    return m_left_margin + column * (m_width + m_spacing);
+}
+
 void WidthProvider::refresh() {
-    auto old = m_width;
-    if (m_minimum <= 0 || m_total <= 0) {
+    auto old   = m_width;
+    auto total = m_total - m_left_margin - m_right_margin;
+
+    if (m_minimum <= 0 || total <= 0) {
         return;
     }
 
-    auto d     = (double)m_total / (m_minimum + m_spacing);
-    auto count = (u64)(d);
+    auto d     = (double)total / (m_minimum + m_spacing);
+    auto count = (i64)(d);
 
     if (count == 0) {
-        m_width = std::max(m_total, m_minimum);
+        m_width = std::max(total, m_minimum);
     } else {
-        auto spacing = (double)m_spacing / m_total;
+        auto spacing = (double)m_spacing / total;
         auto scale   = (d - count) / count + 1.0 + spacing;
         m_width      = m_minimum * scale;
     }
     if (old != m_width) {
         widthChanged();
+    }
+    count = std::max<i64>(count, 1);
+    if (count != m_column) {
+        m_column = count;
+        columnChanged();
     }
 }
 
