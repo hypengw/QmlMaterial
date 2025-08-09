ccb2fa0520567d35de1165596be4b1b71ea70917
hypengw <hypengwip@gmail.com>
2025-08-09 08:15:30 +0000

    cmp_set

--- include/qml_material/core.hpp
+++ include/qml_material/core.hpp
@@ -6,6 +6,7 @@
 #include <memory>
 #include <span>
 #include <utility>
+#include <cmath>
 
 using i8  = std::int8_t;
 using i16 = std::int16_t;
@@ -27,22 +28,47 @@ using const_voidp = const void*;
 template<typename T, typename D = std::default_delete<T>>
 using up = std::unique_ptr<T, D>;
 
-namespace ycore 
+namespace ycore
 {
 
-template <class F>
+template<typename T>
+[[nodiscard]] constexpr bool fuzzy_equal(T a, T b) {
+    const T scale = std::same_as<T, double> ? T(1000000000000.) : T(100000.f);
+    return std::fabs(a - b) * scale <= std::min(std::fabs(a), std::fabs(b));
+}
+
+template<typename T>
+using param_t = std::conditional_t<std::is_trivially_copyable_v<T> && sizeof(T) <= 32, T, const T&>;
+
+template<typename T>
+auto cmp_set(T& lhs, param_t<T> rhs) noexcept -> bool {
+    if constexpr (std::is_floating_point_v<T>) {
+        if (! fuzzy_equal(lhs, rhs)) {
+            lhs = rhs;
+            return true;
+        }
+    } else {
+        if (lhs != rhs) {
+            lhs = rhs;
+            return true;
+        }
+    }
+    return false;
+}
+
+template<class F>
 struct y_combinator {
     F f; // the lambda will be stored here
-    
+
     // a forwarding operator():
-    template <class... Args>
+    template<class... Args>
     decltype(auto) operator()(Args&&... args) const {
         // we pass ourselves to f, then the arguments.
         return f(*this, std::forward<Args>(args)...);
     }
 };
 
-template <class F>
+template<class F>
 y_combinator(F) -> y_combinator<F>;
 
-}
+} // namespace ycore
